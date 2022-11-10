abea6e3737930ae287f2ec7e21d7824af9e37573
Fernando Mendoza <lfmendoza.alvarez@gmail.com>
2022-11-10 09:45:46 -0600

    Agrega clase para controlador de Radio

---
+++ Radio.java
@@ -0,0 +1,50 @@
+public class Radio implements Acciones {
+  //Modo radio
+  public String cambiarFmAm() {
+    return "";
+  }
+  public String cambiarEmisora(String operador) {
+    return "";
+  }
+  public String guardarEmisora(String emisora) {
+    return "";
+  }
+  public String cargarEmisora(int emisora) {
+    return "";
+  }
+  public String seleccionarLista() {
+    return "";
+  }
+  public String cambiarCancion(int direccion) {
+    return "";
+  }
+  public String escucharCancion(int i) {
+    return "";
+  }
+  
+  //------------------------------------------------------------------------------------------------------------------------------------------------
+  //Modo telefono
+
+  public boolean conectarDesconectar() {
+    return true;
+  }
+  public String contactos() {
+    return "";
+  }
+  public String llamar(int contacto) {
+    return "";
+  }
+  public String finalizarLlamada() {
+    return "";
+  }
+  public String llamarUltimoContacto() {
+    return "";
+  }
+  
+  //------------------------------------------------------------------------------------------------------------------------------------------------
+  //Modo productividad
+  
+  public String verTarjetasPresentacion() {
+    return "";
+  }
+}
