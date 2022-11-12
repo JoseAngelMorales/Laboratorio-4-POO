54f186713a4e01349dd413e3b83532770e904832
JoseAngelMorales <105880422+JoseAngelMorales@users.noreply.github.com>
2022-11-11 18:38:04 -0600

    Add files via upload

--- Radio.java
+++ Radio.java
@@ -1,73 +1,82 @@
-public class Radio implements Acciones {
+public class Radio implements Acciones2 {
 
-  String frecuencia;
-
-  Radio(String frecuencia) {
-    this.frecuencia = frecuencia;
-  }
-
-  Radio() {
-    this.frecuencia = "FM";
-  }
-
-  public void setFrecuencia(String frecuencia) {
-    this.frecuencia = frecuencia;
-  }
-
-  public String getFrecuencia() {
-    return this.frecuencia;
-  }
-  //Modo radio
-  public String cambiarFmAm() {
-    if(this.frecuencia.equals("FM")) {
-      setFrecuencia("AM");
-    } else if (this.frecuencia.equals("AM")){
-      setFrecuencia("FM");
+    private String frecuencia;
+    private int volumen;
+  
+    Radio(String frecuencia) {
+      this.frecuencia = frecuencia;
     }
-    return "La frecuencia ahora cambió a " + this.getFrecuencia();
-  }
-  public String cambiarEmisora(String operador) {
-    return "";
-  }
-  public String guardarEmisora(String emisora) {
-    return "";
-  }
-  public String cargarEmisora(int emisora) {
-    return "";
-  }
-  public String seleccionarLista() {
-    return "";
-  }
-  public String cambiarCancion(int direccion) {
-    return "";
-  }
-  public String escucharCancion(int i) {
-    return "";
-  }
   
-  //------------------------------------------------------------------------------------------------------------------------------------------------
-  //Modo telefono
+    Radio() {
+      this.frecuencia = "FM";
+    }
+  
+    public void setFrecuencia(String frecuencia) {
+      this.frecuencia = frecuencia;
+    }
+  
+    public String getFrecuencia() {
+      return this.frecuencia;
+    }
 
-  public boolean conectarDesconectar() {
-    return true;
-  }
-  public String contactos() {
-    return "";
-  }
-  public String llamar(int contacto) {
-    return "";
-  }
-  public String finalizarLlamada() {
-    return "";
-  }
-  public String llamarUltimoContacto() {
-    return "";
-  }
+    public void setVolumen(int volumen){
+        this.volumen = volumen;
+    }
+
+    public int getVolumen(){
+        return this.volumen;
+    }
+    //Modo radio
+    public String cambiarFmAm() {
+      if(this.frecuencia.equals("FM")) {
+        setFrecuencia("AM");
+      } else if (this.frecuencia.equals("AM")){
+        setFrecuencia("FM");
+      }
+      return "La frecuencia ahora cambió a " + this.getFrecuencia();
+    }
+    public String cambiarEmisora(String operador) {
+      return "";
+    }
+    public String guardarEmisora(String emisora) {
+      return "";
+    }
+    public String cargarEmisora(int emisora) {
+      return "";
+    }
+    public String seleccionarLista() {
+      return "";
+    }
+    public String cambiarCancion(int direccion) {
+      return "";
+    }
+    public String escucharCancion(int i) {
+      return "";
+    }
+    
+    //------------------------------------------------------------------------------------------------------------------------------------------------
+    //Modo telefono
   
-  //------------------------------------------------------------------------------------------------------------------------------------------------
-  //Modo productividad
-  
-  public String verTarjetasPresentacion() {
-    return "";
+    public boolean conectarDesconectar() {
+      return true;
+    }
+    public String contactos() {
+      return "";
+    }
+    public String llamar(int contacto) {
+      return "";
+    }
+    public String finalizarLlamada() {
+      return "";
+    }
+    public String llamarUltimoContacto() {
+      return "";
+    }
+    
+    //------------------------------------------------------------------------------------------------------------------------------------------------
+    //Modo productividad
+    
+    public String verTarjetasPresentacion() {
+      return "";
+    }
   }
-}
